28f4d0414ef657b4e549db3fed3ac77de957f9ad
Jessica Cooper <jessicagcooper@gmail.com>
2019-12-13 13:14:51 -0600

    created all helper functions and all MVP api endpoints/http requests.

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -2,5 +2,95 @@ const db = require("../data/db-Config.js")
 
 module.exports = {
    //list helper functions here
- 
+   getProjects,
+   getProjectById,
+   getTasks,
+   getProjectTasks,
+   getResources,
+   getResourceById,
+//    getProjectResources,
+   addProject,
+   addTask,
+   addResource
 }
+
+
+// retrieving a list of projects.
+function getProjects(){
+    return db('projects')
+}
+
+//get project by id
+function getProjectById(id){
+    return db('projects')
+        .where({ id })
+        .first()
+}
+
+//get task by id
+function getTaskById(id){
+    return db('tasks')
+        .where({ id })
+        .first()    
+}
+
+// retrieving a list of tasks. **The list of tasks should include the project name and project description**.
+function getTasks(){
+    return db('projects as p')
+    .select('t.id', 'p.name as project_name', 'p.description as project_description', 't.project_id', 't.description as task_description', 't.notes', 't.completed')
+    .join('tasks as t', 'p.id', 't.project_id')
+}
+
+// get tasks by project id
+function getProjectTasks(project_id){
+    return db('projects as p')
+    .select('t.id', 'p.name as project_name', 'p.description as project_description', 't.project_id', 't.description as task_description', 't.notes', 't.completed')
+    .join('tasks as t', 'p.id', 't.project_id')
+    .where('p.id', project_id)
+}
+
+// retrieving a list of resources
+function getResources(){
+    return db('resources')
+}
+
+//get resource by id
+function getResourceById(id){
+    return db('resources')
+        .where({ id })
+        .first()
+}
+
+// adding projects.
+function addProject(project){
+    return db('projects')
+    .insert(project, 'id')
+    .then(ids => {
+        const [id] = ids;
+
+        return getProjectById(id);
+    })
+}
+
+// adding tasks.
+function addTask(task){
+    return db('tasks')
+    .insert(task, 'id')
+    .then(ids => {
+        const [id] = ids;
+
+        return getTaskById(id);
+    })
+}
+
+// adding resources.
+function addResource(resource){
+    return db('resources')
+    .insert(resource, 'id')
+    .then(ids => {
+        const [id] = ids;
+
+        return getResourceById(id);
+    })
+}
+
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -6,6 +6,72 @@ const router = express.Router();
 
 
 //build http requests here
+router.get('/', (req, res) => {
+    Projects.getProjects()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get projects.', err });
+    });
+  });
+
+  router.get('/tasks', (req, res) => {
+    Projects.getTasks()
+    .then(tasks => {
+      res.json(tasks);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get tasks.', err });
+    });
+  });
+
+  router.get('/resources', (req, res) => {
+    Projects.getResources()
+    .then(resources => {
+      res.json(resources);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get resources.', err });
+    });
+  });
+
+  router.post('/', (req, res) => {
+    const projectData = req.body;
+  
+    Projects.addProject(projectData)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to create new project.', err });
+    });
+  });
+
+  router.post('/tasks', (req, res) => {
+    const taskData = req.body;
+  
+    Projects.addTask(taskData)
+    .then(task => {
+      res.status(201).json(task);
+    })
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to create new task.', err });
+    });
+  });
+
+  router.post('/resources', (req, res) => {
+    const resourceData = req.body;
+  
+    Projects.addResource(resourceData)
+    .then(resource => {
+      res.status(201).json(resource);
+    })
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to create new resource.', err });
+    });
+  });
+
 
 
 module.exports = router;
