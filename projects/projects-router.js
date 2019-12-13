46b81b5ab3c7f9f5f0d75754bac4c237a77c546a
Jessica Cooper <jessicagcooper@gmail.com>
2019-12-13 10:36:13 -0600

    Created package.json, set up projects directory & files, index.js, server.js, dbConfig.js, & knexfile.

---
+++ projects/projects-router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const Projects = require('./projects-model.js');
+
+const router = express.Router();
+
+
+//build http requests here
+
+
+module.exports = router;
